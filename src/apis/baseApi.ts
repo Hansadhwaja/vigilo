0db8c57f11a4ca832e614036836b5e1cefbe48f0
ravi-kanuri <kanuriravi2001@gmail.com>
2026-04-10 16:02:10 +0530

    url change

--- src/apis/baseApi.ts
+++ src/apis/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://vigilo-backend-1.onrender.com/api/v1",
+    baseUrl: "https://v-backend-flae.onrender.com/api/v1",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) headers.set("Authorization", `Bearer ${token}`);
